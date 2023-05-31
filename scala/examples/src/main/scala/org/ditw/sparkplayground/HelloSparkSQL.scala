7bcb4e04255c7bc4d0e2f9d7be18c34beed50ea2
Jiaji Wu <jiaji.wu@volvocars.com>
2023-05-31 09:56:55 +0200

    fix

--- scala/examples/src/main/scala/org/ditw/sparkplayground/HelloSparkSQL.scala
+++ scala/examples/src/main/scala/org/ditw/sparkplayground/HelloSparkSQL.scala
@@ -1,18 +1,34 @@
 package org.ditw.sparkplayground
 
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.ditw.sparkplayground.utils.SparkallUtils
 
 object HelloSparkSQL {
   def main(args:Array[String]):Unit = {
 
-    val context = SparkallUtils.localContext("Hello Spark SQL")
+    val session = SparkallUtils.localSession("Hello Spark SQL")
+    import session.implicits._
+    val rdd = session.sparkContext.parallelize(1 to 10).map(i => (i, s"#${i}"))
+    val df = rdd.toDF("index", "value")
+    println("========= schema: ")
+    df.printSchema()
+    println("========= data: ")
+    df.show()
 
-    val sum = context.parallelize(Array(1, 2))
-      .sum()
-    println(s"sum: $sum")
+    val schema = StructType(Array(
+      StructField("index", IntegerType, false),
+      StructField("value", StringType, false)
+    ))
 
-    context.stop()
+    val df2 = session.createDataFrame(rdd.map(tp => Row(tp._1, tp._2)), schema)
+    println("========= schema: ")
+    df2.printSchema()
+    println("========= data: ")
+    df2.show()
+
+    session.stop()
   }
 
 }

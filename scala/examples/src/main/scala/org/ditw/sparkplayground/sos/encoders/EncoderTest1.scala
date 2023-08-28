5346ef72e135b10053caf8f8f570393697633319
Jiaji Wu <jiaji.wu@volvocars.com>
2023-08-28 14:00:06 +0200

    encoder tests

--- scala/examples/src/main/scala/org/ditw/sparkplayground/sos/encoders/EncoderTest1.scala
+++ scala/examples/src/main/scala/org/ditw/sparkplayground/sos/encoders/EncoderTest1.scala
@@ -91,6 +91,7 @@ object EncoderTest1 {
     val slzr = boolEncoder.createSerializer()
     println(s"('Ok', 'Now'): ${slzr.apply(StrStr("Ok", "Now"))}")
     println(s"('Ok',    ''): ${slzr.apply(StrStr("Ok", ""))}")
+    println(s"('OkOkOkO', 'Now'): ${slzr.apply(StrStr("OkOkOkO", "Now"))}")
     println(s"('OkOkOkOk', 'Now'): ${slzr.apply(StrStr("OkOkOkOk", "Now"))}")
     println(s"('OkOkOkOkO', 'Now'): ${slzr.apply(StrStr("OkOkOkOkO", "Now"))}")
     val nul1 = slzr.apply(StrStr("Ok", "Now"))
@@ -100,6 +101,41 @@ object EncoderTest1 {
     val nul2 = slzr.apply(StrStr("Ok", ""))
     nul2.setNullAt(0)
     println(s"('Ok',    '') -> (null, ''): $nul2")
+
+    // val dszr = boolEncoder.createDeserializer()
+    val dszr = boolEncoder.resolveAndBind().createDeserializer()
+    val strstr1 = slzr.apply(StrStr("Ok", "Now"))
+    val strstr1Back = dszr.apply(strstr1)
+    assert(strstr1Back == StrStr("Ok", "Now"))
+
+  }
+
+  private def testStrStrComments(): Unit = {
+    val boolEncoder = implicitly[ExpressionEncoder[StrStr]]
+
+    // def createSerializer(): Serializer[T] = new Serializer[T](optimizedSerializer)
+    // class Serializer[T](private val expressions: Seq[Expression])
+    //    extends (T => InternalRow) with Serializable { ... }
+    val slzr = boolEncoder.createSerializer()
+
+    // def createDeserializer(): Deserializer[T] = new Deserializer[T](optimizedDeserializer)
+    // class Deserializer[T](private val expressions: Seq[Expression])
+    //    extends (InternalRow => T) with Serializable { ... }
+    val dszr = boolEncoder.resolveAndBind().createDeserializer()
+
+    // case class StrStr
+    //  serializer:
+    //    slzr = {ExpressionEncoder$Serializer@8580} <function1>
+    //     expressions = {$colon$colon@11378} size = 2
+    //      0 = {Alias@11384} staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(assertnotnull(input[0, org.ditw.sparkplayground.sos.encoders.StrStr, true])).s1, true, false, true) AS s1#6
+    //      1 = {Alias@11385} staticinvoke(class org.apache.spark.unsafe.types.UTF8String, StringType, fromString, knownnotnull(assertnotnull(input[0, org.ditw.sparkplayground.sos.encoders.StrStr, true])).s2, true, false, true) AS s2#7
+    //     inputRow = {GenericInternalRow@11379} [StrStr(Ok,)]
+    //     extractProjection = {GeneratedClass$SpecificUnsafeProjection@11380} "<function1>"
+    //  deserializer:
+    //    dszr = {ExpressionEncoder$Deserializer@8582} <function1>
+    //     expressions = {$colon$colon@11351} size = 1
+    //      0 = {NewInstance@11353} newInstance(class org.ditw.sparkplayground.sos.encoders.StrStr)
+    //     constructProjection = null
   }
 
   def main(args: Array[String]): Unit = {
